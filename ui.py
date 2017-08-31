c161ecd390331398173080ca69f409522dc0afec
Benjamin Sauder <13512160+BenjaminSauder@users.noreply.github.com>
2017-08-31 21:49:14 +0200

    * added a menu, for nwo resides under the special menu

--- ui.py
+++ ui.py
@@ -1,7 +1,6 @@
 import bpy
 
 import workplane.operator      
-import workplane.draw
 
 class WorkplanePanel():
     
@@ -9,16 +8,11 @@ class WorkplanePanel():
         self.init = False
 
     def draw(self, context):
-        #this is a little hacky, i dont know where to properly autostart this..
-        #in the UI the restricted_context seems to be not an issue        
-        workplane.draw.setup()
-
         layout = self.layout            
 
         col = layout.column(align=True)        
         col.operator("transform.workplane_set", text="Set workplane")
-        #col.operator("transform.workplane_disable", text="Disable workplane")
-                
+      
         col = layout.column(align=True)      
         row = col.row(align=True)
         row.prop_enum(context.scene.workplane, "preview_mode", 'FULL')
@@ -39,11 +33,10 @@ class WorkplanePanel():
         col.operator("transform.workplane_scale", text="Scale") 
         col.operator("transform.workplane_extrude", text="Extrude")
 
-              
-              
+
 class WorkplanePanelTransform(WorkplanePanel, bpy.types.Panel):
     """Workplane Tools Panel"""
-    bl_label = "Workplane "
+    bl_label = "Workplane"
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'TOOLS'
     bl_category = "Tools"
@@ -58,3 +51,42 @@ class WorkplanePanelMeshEdit(WorkplanePanel, bpy.types.Panel):
     bl_category = "Tools"
     bl_context = "mesh_edit"    
     bl_label = "Workplane"
+         
+class WorkplanePanelMenu(bpy.types.Menu):
+    bl_label = "Workplane"
+    bl_idname = "VIEW3D_MT_workplane_menu"     
+
+    def draw(self, context):
+        layout = self.layout            
+
+        col = layout.column(align=True)        
+        col.operator("transform.workplane_set", text="Set workplane")
+
+        layout.separator() 
+        col = layout.column(align=True)      
+        #row = col.row(align=True)
+        col.prop_enum(context.scene.workplane, "preview_mode", 'FULL')
+        col.prop_enum(context.scene.workplane, "preview_mode", 'SIMPLE')
+        col.enabled = workplane.operator.working_in_workplane(context) 
+        
+        col = layout.column(align=True) 
+        col.prop(context.scene.workplane, "active", toggle=False, text="Use Workplane") 
+        col.enabled = workplane.operator.has_workplane(context) 
+
+        col = layout.column(align=True) 
+        col.prop(context.scene.workplane, "visible", toggle=False, text="Display Workplane") 
+        col.enabled = workplane.operator.working_in_workplane(context) 
+
+        layout.separator()
+        col = layout.column(align=True)        
+        col.operator("transform.workplane_translate", text="Translate")
+        col.operator("transform.workplane_rotate", text="Rotate")
+        col.operator("transform.workplane_scale", text="Scale") 
+        col.operator("transform.workplane_extrude", text="Extrude")
+
+
+              
+# draw function for integration in menus
+def menu_func(self, context):
+    self.layout.separator()
+    self.layout.menu("VIEW3D_MT_workplane_menu")
